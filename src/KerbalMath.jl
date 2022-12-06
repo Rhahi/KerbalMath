909f4443be322f59fdf6996c032dd2f0e36d3673
Rhahi <git@rhahi.com>
2022-12-07 00:49:00 +0100

    fix: add missing exports for conversions

--- src/KerbalMath.jl
+++ src/KerbalMath.jl
@@ -7,6 +7,7 @@ import Base: +, -
 
 export ∠θ, ×ₗ, crossₗ, hat
 export planar_rotate, axial_rotate, axial_rotateₗ, angle_direction, angle_directionₗ
+export V2T, T2V, F32, F64, I32, I64, UI32
 
 include("conversions.jl")
 include("linearalgebra.jl")
